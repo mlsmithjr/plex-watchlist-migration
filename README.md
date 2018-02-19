5480a6d9a4b56bfbe7f21aabb8911ef9c91153a3
Mark Smith <marshallsmithjr@gmail.com>
2018-02-18 20:16:46 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # plex-watchlist-migration
 Script to migrate your "watch" list of media to a new Plex server.
+
+This is a first-class quick hack job.  Follow the instructions precisely.
+
+This utility is intended to migrate over your user media viewing history when you move to a new
+Plex server instance.  The process of copying around the full database folder structure is not reliable and
+is just trouble.  So this tool assumes you are starting new libraries on the new machine.
+After copying over all your media, instruct the new Plex server to scan and rebuild your metadata.
+Yes, this will take quite a bit of time for large collections.
+#
+After it is done, shut down Plex.
+
+The primary database file for Plex is called com.plexapp.plugins.library.db and is located here:
+  Windows: c:/Users/YOUR_USER/AppData/Local/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db
+  Linux: /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/com.plexapp.plugins.library.db
+  MacOS: /Users/YOUR_USER/Library/Application Support/Plex Media Server/Plug-in Support/com.plexapp.plugins.library.db
+
+ 1. Create a working directory somewhere. This is where you will run the script.
+ 2. Locate the database file of the NEW plex server and copy it to your working directory as "new.db".
+ 3. Locate the main database file of the OLD plex server and copy it to your working directory as "old.db"
+ 4. Put this script in the working directory.
+ 5. In your work directory you should have: export.py (this file), old.db, new.db
+ 6. At a shell prompt, in the work directory, execute "python3 export.py"
+ 7. You will probably get a bunch of messages about missing guids.  This is cruft built up in your database over time,
+    usually media you don't have anymore. You can either research them yourself to be sure or continue.
+ 8. Make you have an unaltered backup copy of your new plex database file in case this goes wrong.
+ 9. Finally, copy new.db over your new plex server's main database file (as com.plexapp.plugins.library.db).
+10. Start Plex and verify.  IF you have any concerns just replace the database file with the backup copy you hopefully made.
+
---
+++ export.py
@@ -0,0 +1,110 @@
+
+#
+# This is a first-class quick hack job.  Follow the instructions precisely.
+#
+# This utility is intended to migrate over your user media viewing history when you move to a new
+# Plex server instance.  The process of copying around the full database folder structure is not reliable and
+# is just trouble.  So this tool assumes you are starting new libraries on the new machine.
+# After copying over all your media, instruct the new Plex server to scan and rebuild your metadata.
+# Yes, this will take quite a bit of time for large collections.
+#
+# After it is done, shut down Plex.
+#
+# The primary database file for Plex is called com.plexapp.plugins.library.db and is located here:
+#  Windows: c:/Users/YOUR_USER/AppData/Local/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db
+#  Linux: /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/com.plexapp.plugins.library.db
+#  MacOS: /Users/YOUR_USER/Library/Application Support/Plex Media Server/Plug-in Support/com.plexapp.plugins.library.db
+##
+# 1. Create a working directory somewhere. This is where you will run the script.
+# 2. Locate the database file of the NEW plex server and copy it to your working directory as "new.db".
+# 3. Locate the main database file of the OLD plex server and copy it to your working directory as "old.db"
+# 4. Put this script in the working directory.
+# 5. In your work directory you should have: export.py (this file), old.db, new.db
+# 6. At a shell prompt, in the work directory, execute "python3 export.py"
+# 7. You will probably get a bunch of messages about missing guids.  This is cruft built up in your database over time,
+#    usually media you don't have anymore. You can either research them yourself to be sure or continue.
+# 8. Make you have an unaltered backup copy of your new plex database file in case this goes wrong.
+# 9. Finally, copy new.db over your new plex server's main database file (as com.plexapp.plugins.library.db).
+# 10. Start Plex and verify.  IF you have any concerns just replace the database file with the backup copy you hopefully made.
+
+import sqlite3
+
+oldcon = sqlite3.connect('old.db')
+newcon = sqlite3.connect('new.db')
+oldcur = oldcon.cursor()
+newcur = newcon.cursor()
+
+#
+# copy over the users
+#
+rows = oldcur.execute('select id,name,hashed_password,salt,created_at from accounts')
+usermap = []
+for row in rows:
+    (oldid,name,hashed_password,salt,created_at) = row
+    if oldid > 1:       # if PlexPass and have non-admin users, bring them over
+        newcur.execute('insert into accounts (name,hashed_password,salt,created_at) values (?,?,?,?)', row[1:])
+        newid = newcur.lastrowid
+    else:
+        newid = oldid
+    usermap.append((oldid, newid))
+
+#
+# get list of all guids in the new system for validation
+#
+guidlist = [row[0] for row in newcur.execute('select guid from metadata_items')]
+
+#
+# get list of old library sections for mapping to new ones
+#
+oldsections = dict()
+for row in oldcur.execute('select id,name from library_sections'):
+    (id,name) = row
+    oldsections[id] = { 'name': name }
+
+#
+# map to new sections
+#
+for id,values in oldsections.items():
+    for row in newcur.execute('select id from library_sections where name = ?', (values['name'],)):
+        values['newid'] = row[0]
+
+#
+# get list of things the users have watched
+#
+
+count = 0
+for user in usermap:
+    (oldid, newid) = user
+    watchlist = oldcur.execute(
+        "select account_id,guid,metadata_type,library_section_id,grandparent_title,"
+        "parent_index,parent_title,'index',title,thumb_url,viewed_at,grandparent_guid,originally_available_at "
+        "from metadata_item_views where account_id=? and library_section_id is not null", (oldid,))
+
+    for watched in watchlist:
+        guid = watched[1]
+        if not guid in guidlist:
+            print('guid ' + watched[1] + ' not found - skipped')
+            continue
+
+        library_section_id = watched[3]
+        if library_section_id in oldsections.keys():
+            library_section_id = oldsections[library_section_id]['newid']
+        else:
+            print('unknown library section {} for guid {} - skipped'.format(library_section_id, guid))
+            continue
+
+        w = list(watched)
+        w[0] = newid
+        w[3] = library_section_id
+        watched = tuple(w)
+        newcur.execute("insert into metadata_item_views ("
+                "account_id,guid,metadata_type,library_section_id,grandparent_title,"
+                "parent_index,parent_title,'index',title,thumb_url,viewed_at,grandparent_guid,"
+                "originally_available_at) values (?,?,?,?,?,?,?,?,?,?,?,?,?)", watched)
+        count += 1
+
+oldcur.close()
+newcur.close()
+oldcon.commit()
+newcon.commit()
+print('migrated {} watch records for {} users'.format(count, len(usermap)))
