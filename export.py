7b9ba2e4b42c3a15641fc578185f84e71cd06ac0
Mark Smith <marshallsmithjr@gmail.com>
2018-02-19 18:32:11 -0500

    Improved duplicate prevention on users (accounts) and viewed items.

--- export.py
+++ export.py
@@ -5,7 +5,9 @@
 # This utility is intended to migrate over your user media viewing history when you move to a new
 # Plex server instance.  The process of copying around the full database folder structure is not reliable and
 # is just trouble.  So this tool assumes you are starting new libraries on the new machine.
-# After copying over all your media, instruct the new Plex server to scan and rebuild your metadata.
+# After copying over all your media, instruct the new Plex server to scan and rebuild your metadata. Do this for all your
+# libraries before proceeding.  This script should be your final step.
+#
 # Yes, this will take quite a bit of time for large collections.
 #
 # After it is done, shut down Plex.
@@ -15,6 +17,8 @@
 #  Linux: /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/com.plexapp.plugins.library.db
 #  MacOS: /Users/YOUR_USER/Library/Application Support/Plex Media Server/Plug-in Support/com.plexapp.plugins.library.db
 ##
+# This script is rerun-safe. You can run it as many times as you want and you shouldn't get duplicate data.
+#
 # 1. Create a working directory somewhere. This is where you will run the script.
 # 2. Locate the database file of the NEW plex server and copy it to your working directory as "new.db".
 # 3. Locate the main database file of the OLD plex server and copy it to your working directory as "old.db"
@@ -90,10 +94,24 @@ for user in usermap:
         "parent_index,parent_title,'index',title,thumb_url,viewed_at,grandparent_guid,originally_available_at "
         "from metadata_item_views where account_id=? and library_section_id is not null", (oldid,))
 
+    newwatchlist = newcur.execute(
+        "select guid from metadata_item_views where account_id=? and library_section_id is not null", (newid,))
+
+    #
+    # make a list of already watched items in the new database, in case this script is being rerun we don't want duplicates
+    #
+    already_watched = []
+    for newwatch in newwatchlist:
+        already_watched.append(newwatch[0])
+
     for watched in watchlist:
         guid = watched[1]
+        if guid in already_watched:
+            # already been registered, skip
+            continue
+
         if not guid in guidlist:
-            print('guid ' + watched[1] + ' not found - skipped')
+            print('guid ' + guid + ' not found in media list - skipped')
             continue
 
         library_section_id = watched[3]
