08d2235ebeae46ffed4b925a2baecc7e27e58c8b
Mark Smith <marshallsmithjr@gmail.com>
2021-10-14 21:11:35 -0400

    Removed unworking export.py and replaced with watchlist.py, reworked README

--- README.md
+++ README.md
@@ -1,46 +1,37 @@
 # plex-watchlist-migration
-Script to migrate your "watch" list of media to a new Plex server.
-
-### This is a first-class quick hack job.  Follow the instructions precisely.
-
-### Tested on Plex Media Server versions:
-* 1.11.3.4803
-* 1.10.1.4602
+Script to migrate your "watched" list of media to a new Plex server.
 
 *Features*
 * Migrate missing users.
 * Migrate watch history of users.
 * Migrate date added/created to preserve on-deck ordering
 
-
 This utility is intended to migrate over your user media viewing history when you move to a new
-Plex server instance.  The process of copying around the full database folder structure is not reliable and
+Plex server instance or to just maintain a failover.  The process of copying around the full database folder structure is not reliable and
 is just trouble.  So this tool assumes you are starting new libraries on the new machine.
 After copying over all your media, instruct the new Plex server to scan and rebuild your metadata.
 Yes, this will take quite a bit of time for large collections.  Do this for all your
 libraries before proceeding.  This script should be your final step.
 
-After it is done, shut down Plex.
+You may start an export on a running Plex server, but you *must* stop the Plex server on the destination side before loading the export.
 
-The primary database file for Plex is called **com.plexapp.plugins.library.db** and is located here:
+Export from your primary (source) Plex database:
 
-	  Windows: c:/Users/YOUR_USER/AppData/Local/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db
-	  Linux: /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-in Support/com.plexapp.plugins.library.db
-	  MacOS: /Users/YOUR_USER/Library/Application Support/Plex Media Server/Plug-in Support/com.plexapp.plugins.library.db
+    python3 watchlist.py -e <source db>  <export file>
 
-### This script is rerun-safe. You can run it as many times as you want and you shouldn't get duplicate data.
-
-1.  Create a working directory somewhere. This is where you will run the script.
-1.  Locate the database file of the NEW plex server and copy it to your working directory as "new.db".
-1.  Locate the main database file of the OLD plex server and copy it to your working directory as "old.db"
-1.  Put this script in the working directory.
-1.  In your work directory you should have: export.py (this file), old.db, new.db
-1.  At a shell prompt, in the work directory, execute `python3 export.py`
-1.  You will probably get a bunch of messages about missing guids.  This is cruft built up in your database over time, usually media you don't have anymore. You can either research them yourself to be sure or continue.
-!.  You may rerun the script as often as you like as it will not create duplicate watch records.
-1.  Make you have an unaltered backup copy of your new plex database file in case this goes wrong.
-1.  Finally, copy new.db over your new plex server's main database file (as com.plexapp.plugins.library.db).
-1. Start Plex and verify.  IF you have any concerns just replace the database file with the backup copy you hopefully made.
+    ex: python3 watchlist.py -e /mnt/data/Plex/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Databases/com.plexapp.plugins.library.db /tmp/export.json
 
 
-If anyone wants to contribute to make this script a full blow robust tool I'm all in.
+When ready to import into the destination Plex db, *stop the Plex server first* or you may corrupt the database.
+
+    python3 watchlist.py -i <export file>  <destination db>
+
+    ex: python3 watchlist.py -i export.json /mnt/data/Plex/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Databases/com.plexapp.plugins.library.db
+
+
+This script is rerun-safe. You can run it as many times as you want, and you shouldn't get duplicate data.
+
+### NOTE: If you get a bunch of warnings during import listing media files not found on the destination it could be:
+1. Old media garbage data in the source Plex db.
+2. Incompatible _guid_ values between source and destination.  When Plex switched to their own scrapers the guid values changed, which were being used as primary keys in the database. To fix you can try refreshing full metadata of the affected item(s) and re-export.
+
---
+++ watchlist.py
@@ -0,0 +1,246 @@
+import sqlite3
+import sys
+import json
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def do_export(source_db, export_filename):
+    db_connection = sqlite3.connect(source_db)
+    db_connection.row_factory = dict_factory
+    cursor = db_connection.cursor()
+
+    #
+    # get users
+    #
+    rows = cursor.execute('select id,name,hashed_password,salt,created_at from accounts where id != 0')
+    user_list = []
+    for user in rows.fetchall():
+
+        print(f"Exporting for user {user['name']}")
+        user_list.append(user)
+
+        #
+        # get the watchlist for user
+        #
+        user["watchlist"] = []
+        watchlist = cursor.execute(
+            "select account_id,guid,metadata_type,library_section_id,grandparent_title,"
+            "parent_index,parent_title,'index',title,thumb_url,viewed_at,grandparent_guid,originally_available_at "
+            "from metadata_item_views inner join library_sections on library_sections.id = metadata_item_views.library_section_id "
+            "where account_id=? and library_sections.section_type in (1,2)", (user["id"],))
+        for row in watchlist.fetchall():
+            user["watchlist"].append(row)
+        print(f"  {len(user['watchlist'])} watched items")
+
+        user["metadata_item_settings"] = {}
+        settings = cursor.execute(
+            'select account_id,guid,rating,view_offset,view_count,last_viewed_at,created_at,'
+            'skip_count,last_skipped_at,changed_at,extra_data '
+            'from metadata_item_settings '
+            'where account_id = ?', (user["id"],))
+        for row in settings.fetchall():
+            guid = row["guid"]
+            user["metadata_item_settings"][guid] = row
+#            user["metadata_item_settings"].append(row)
+
+    #
+    # get list of source library sections for mapping to new ones
+    #
+    source_sections = dict()
+    for row in cursor.execute('select id,name from library_sections where section_type in (1,2)').fetchall():
+        (_id, name) = row.values()
+        source_sections[_id] = {'name': name}
+
+    #
+    # get list of media added dates to preserve on-deck ordering in destination system
+    #
+    ordering = cursor.execute(
+        'select guid,added_at,metadata_items.created_at from metadata_items '
+        'inner join library_sections on library_sections.id = metadata_items.library_section_id '
+        'where library_sections.section_type in (1,2)').fetchall()
+
+    full_export = {"users": user_list, "oldsections": source_sections, "ordering": ordering}
+    with open(export_filename, "w") as fp:
+        json.dump(full_export, fp, indent=2)
+    print("export complete, results in export.json")
+
+
+def do_import(export_filename, dest_db):
+    db_connection = sqlite3.connect(dest_db)
+    db_connection.row_factory = dict_factory
+    cursor = db_connection.cursor()
+
+    with open(export_filename, "r") as fp:
+        source = json.load(fp)
+
+    orig_userlist = source["users"]
+    source_sections = source["oldsections"]
+    ordering = source["ordering"]
+
+    #
+    # Get list of all guids in the new system for validation.
+    #
+    dest_guid_list = [row["guid"] for row in cursor.execute(
+        'select guid from metadata_items mi inner join library_sections ls on mi.library_section_id = ls.id where ls.section_type in (1,2)')]
+
+    #
+    # map source to destination sections
+    #
+    for values in source_sections.values():
+        for row in cursor.execute('select id from library_sections where name = ?', (values['name'],)):
+            values['newid'] = row["id"]
+        if "newid" not in values:
+            print('section name not found in database - {}'.format(values['name']))
+
+    #
+    # create a map of source to destination user ids
+    #
+    for orig_user in orig_userlist:
+        print(f"Processing user {orig_user['name']}")
+        if orig_user["id"] > 1:  # if PlexPass and have non-admin users, bring them over
+            # check if user already exists
+            cursor.execute('select id, name from accounts where name = ?', (orig_user["name"],))
+            existing = cursor.fetchone()
+            if existing is None:
+                # create user
+                cursor.execute('insert into accounts (name,hashed_password,salt,created_at) values (?,?,?,?)',
+                               (orig_user["name"], orig_user["hashed_password"], orig_user["salt"],
+                                orig_user["created_at"]))
+                dest_user_id = cursor.lastrowid
+            else:
+                # map account id to existing user
+                dest_user_id = existing["id"]
+        else:
+            # probably just the main admin user (account id 1)
+            dest_user_id = orig_user["id"]
+
+        #
+        # create map of source to destination guid and settings
+        #
+        source_settings = dict()
+        for setting in orig_user["metadata_item_settings"].values():
+            guid = setting["guid"]
+            source_settings[guid] = setting
+
+        #
+        # get list of things the user already watched in the destination so we don't duplicate
+        #
+        count = 0
+        dest_watchlist = cursor.execute(
+            "select guid from metadata_item_views inner join library_sections on library_sections.id = metadata_item_views.library_section_id "
+            "where account_id=? and library_sections.section_type in (1,2)", (dest_user_id,)).fetchall()
+
+        already_watched = [watched["guid"] for watched in dest_watchlist]
+
+        guid_dedup = []
+
+        #
+        # iterate over each watched item from the source system
+        #
+        for watched in orig_user["watchlist"]:
+            guid = watched["guid"]
+            gptitle = watched["grandparent_title"]
+            ptitle = watched["parent_title"]
+            title = watched["title"]
+
+            if guid in guid_dedup:
+                # clean up dups in watch list while we are here
+                continue
+
+            if guid in already_watched:
+                # already been registered, skip
+                continue
+
+            if guid not in dest_guid_list:
+                print(f'  {gptitle} {ptitle} {title} (guid {guid}) not found in media list - skipped')
+                continue
+
+            library_section_id = str(watched["library_section_id"])
+            if library_section_id in source_sections.keys():
+                library_section_id = source_sections[library_section_id]["newid"]
+            else:
+                print('  unknown library section {} for guid {} - skipped'.format(library_section_id, guid))
+                continue
+
+            if guid not in source_settings:
+                print(
+                    '  Unexpected: guid {} not found in metadata_item_settings for user {}'.format(guid, orig_user["id"]))
+                continue
+            #
+            # everything looks good, insert rows
+            #
+
+            # TODO: refactor this; it assumes ordering of values
+            w = list(watched.values())
+            w[0] = dest_user_id
+            w[3] = library_section_id
+            watched = tuple(w)
+
+            cursor.execute("insert into metadata_item_views ("
+                           "account_id,guid,metadata_type,library_section_id,grandparent_title,"
+                           "parent_index,parent_title,'index',title,thumb_url,viewed_at,grandparent_guid,"
+                           "originally_available_at) values (?,?,?,?,?,?,?,?,?,?,?,?,?)", watched)
+
+            # TODO: refactor this; it assumes ordering of values
+            metadata_item_settings = orig_user["metadata_item_settings"]
+            s = list(metadata_item_settings[guid].values())
+            s[0] = dest_user_id
+            settings = tuple(s)
+            cursor.execute(
+                'insert into metadata_item_settings (account_id,guid,rating,view_offset,view_count,'
+                'last_viewed_at,created_at,skip_count,last_skipped_at,changed_at,extra_data) '
+                'values (?,?,?,?,?,?,?,?,?,?,?)', settings)
+
+            guid_dedup.append(guid)
+            count += 1
+
+        if count:
+            print(f"{count} update(s) for {orig_user['name']}")
+        else:
+            print(f"No updates for {orig_user['name']}")
+    #
+    # go through all the media and migrate the added and created dates to preserve "on-deck" ordering
+    #
+
+    #
+    # This is tricky because Plex uses custom C-based tokenizer code which isn't available to developers.
+    # This causes a trigger error when attempting to update metadata_items.
+    # This is my workaround: query the triggers, drop the triggers, do the updates, then recreate the triggers.
+    # I think this is safe because those triggers just maintain text search indexes of titles, which this
+    # code doesn't modify anyhow.
+    #
+
+    # grab the triggers
+    triggers = cursor.execute("select name,sql from sqlite_master where type='trigger' and tbl_name='metadata_items' and name like '%_update_%'").fetchall()
+    # now delete them
+    for trigger in triggers:
+        cursor.execute(f"drop trigger {trigger['name']}")
+
+    for item in ordering:
+        # just do an optimistic update - sqlite will ignore if row doesn't exist
+        cursor.execute('update metadata_items set added_at=?, created_at=? where guid=?',
+                       (item["added_at"], item["created_at"], item["guid"]))
+
+    # recreate the triggers
+    for trigger in triggers:
+        cursor.execute(trigger["sql"])
+
+    cursor.close()
+    db_connection.commit()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("usage: watchlist.py -e <source database filename> <export filename>")
+        print("                    -i <export filename> <destination database filename>")
+        sys.exit(0)
+    if sys.argv[1] == "-e":
+        do_export(sys.argv[2], sys.argv[3])
+    if sys.argv[1] == "-i":
+        do_import(sys.argv[2], sys.argv[3])
